e764ebff2fa4c15625642bc4d598d54e055e46d2
Erik Lindberg <lindbergerik@outlook.com>
2018-04-01 20:20:37 +0200

    personal places

--- interaction.js
+++ interaction.js
@@ -45,25 +45,3 @@ function panRight() {
 // 	console.log("X coords: " + distX + ", Y coords: " + distY);
 // }
 
-function toggleFullScreen() {
-  var doc = window.document;
-  var docEl = doc.documentElement;
-
-  var requestFullScreen = docEl.requestFullscreen || docEl.mozRequestFullScreen || docEl.webkitRequestFullScreen || docEl.msRequestFullscreen;
-  var cancelFullScreen = doc.exitFullscreen || doc.mozCancelFullScreen || doc.webkitExitFullscreen || doc.msExitFullscreen;
-
-  if(!doc.fullscreenElement && !doc.mozFullScreenElement && !doc.webkitFullscreenElement && !doc.msFullscreenElement) {
-    requestFullScreen.call(docEl);
-  }
-  else {
-    cancelFullScreen.call(doc);
-  }
-}
-
-// var coordinates = {lat: 0, lng: 0};
-// function placeMarker(event) {
-// 	var coordinates = {lat: event.clientX, lng: event.clientY};
-// 	map.marker.position=coordinates;
-// 	//map.marker.setMap(map);
-// 	//PLACE MARKER
-// }
